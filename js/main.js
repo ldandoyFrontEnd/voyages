81b601ab584069d782d42718b9b5aa97d141f3af
ldandoyFrontEnd <ldandoy@gmail.com>
2023-07-11 10:43:58 +0200

    MAJ

--- js/main.js
+++ js/main.js
@@ -14,3 +14,35 @@ menuDowns.forEach(dropdown => {
         }
     })
 })
+
+const valid = () => {
+    console.log("test");
+
+    const nameInput = document.querySelector('.nameInput');
+    const emailInput = document.querySelector('.emailInput');
+    const msgElement = document.querySelector('.msg');
+    let msg = "";
+    let validRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    msgElement.style.display = "none";
+    msgElement.innerHTML = "";
+
+    if (nameInput.value.length < 4) {
+        msg += "<div>Le nom est trop petit</div>";
+        nameInput.focus();
+    }
+
+    if ( !emailInput.value.match(validRegexp) ) {
+        msg += "<div>Email mal formaté</div>";
+        emailInput.focus();
+    }
+
+    if (msg == "") {
+        return true;
+    }
+
+    msgElement.innerHTML = msg;
+    msgElement.style.display = "block";
+
+    return false;
+}
